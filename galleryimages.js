0eace523935e1218fab0bd31a5631c72cc2b1a58
Carl Johansson <carljohansson.1992@gmail.com>
2021-11-26 11:36:43 +0100

    button and styling

--- galleryimages.js
+++ galleryimages.js
@@ -2,37 +2,24 @@
 let galleryWrapper = document.querySelector('.gallery-wrapper');
 let popupCont = document.querySelector('.popup-container');
 let jsonImages;
+
 fetchImages(createImage);
 
 function fetchImages(x){
-fetch('/projektarbete-webbproduktion/images.json')
-.then(function (response) {
-    return response.json();
-})
-.then(data => {
-    jsonImages = data;
-    x(data);
-    console.log(jsonImages[3].url);
-})
-.catch(error => console.log(error));
-};
-
-console.log(jsonImages);
-/*function createPopup(){
-    let popUpImg = document.createElement('img');
-    let popUpText = document.createElement('p');
-
-    popUpImg.className = "popup-img";
-    popUpImg.setAttribute('src', image.url);
-        
-    popUpText.className = "popup-text";
-    popUpText.textContent = image.description;
-
-    popupCont.appendChild(popUpImg);
-    popupCont.appendChild(popUpText);
+    fetch('/projektarbete-webbproduktion/images.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(data => {
+        jsonImages = data;
+        x(data);
+    })
+    .catch(error => console.log(error));
 }
-*/
-function createImage(images)    {
+
+
+
+function createImage(images){
     
 images.forEach((image, index) => {
         
@@ -52,85 +39,38 @@ images.forEach((image, index) => {
         img.dataset.index = index;
         img.addEventListener('click', popup);
 
-        //let descriptionElement = document.createElement('p');
-        //descriptionElement.textContent = image.description;
-       // descriptionElement.className = 'gallery-description';
-
-        //let popUpImg = document.createElement('img');
-        //let popUpText = document.createElement('p');
-
-        /*function popup(){
-            
-           
-            popupCont.style.display = "flex";
-        
-            popUpImg.className = "popup-img";
-            popUpImg.setAttribute('src', image.url);
-        
-            popUpText.className = "popup-text";
-            popUpText.textContent = image.description;
-        
-            console.log(image.url);
-        }*/
-
         galleryWrapper.appendChild(testDiv);
         testDiv.appendChild(titleElement);
         testDiv.appendChild(img);
-        //testDiv.appendChild(descriptionElement);
-        //popupCont.appendChild(popUpImg);
-        //popupCont.appendChild(popUpText);
-
-
     });
 }
+
 function popup(e){
     
     let popUpImg = document.createElement('img');
     let currentImg = jsonImages[e.target.dataset.index];
 
     popupCont.style.display = "flex";
-
     popUpImg.className = "popup-img";
     popUpImg.setAttribute('src', currentImg.url);
+
 
     let popUpText = document.createElement('p');
     popUpText.textContent = currentImg.description;
     popUpText.className = "popup-text";
+
+    let quit = document.createElement('button');
+    quit.textContent = "X";
+    quit.className = "quit-button";
+    quit.addEventListener('click', closePop);
     
-    popupCont.replaceChildren(popUpImg, popUpText);
+    popupCont.replaceChildren(popUpImg, popUpText, quit);
     console.log(currentImg.url);
-
 }
-//function popup(){
-    //fetchImages(createPopup);
-    //popupCont.style.display = "flex";
-//}
-//function popup(e){
-   // console.log([0]);
-//}
 
-/*function popup(){
-    
-    popupCont.style.display = "block";
-
-    popUpImg.className = "popup-img";
-    popUpImg.setAttribute('src', image.url);
-
-    popUpText.className = "popup-text";
-    popUpText.textContent = image.description;
-
-    console.log(image.url);
-}*/
-    
-
-        
-    //function popup(){
-    //popupCont.style.display = "block";
-    //console.log(image.url);
-//}
-/* window.addEventListener('click', closePop);
 function closePop(){
+    
     popupCont.style.display = "none";
-    console.log("close");
-} */
+}
+
  
